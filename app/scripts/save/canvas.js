823280badf73a2a319fabf1bb2bedf2610c5ac06
Kyotsuke <mattieu.vendeville@gmail.com>
2016-12-13 10:39:29 +0100

    Corection des erreurs

--- app/scripts/save/canvas.js
+++
@@ -1,7 +0,0 @@
-let canvas = document.getElementById('drawzone');
-let ctx = canvas.getContext('2d');
-
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-
-export { canvas, ctx };
